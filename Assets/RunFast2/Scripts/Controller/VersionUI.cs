1421a2e75083b6adcf14089e16a72c0338696195
ballshe <ballshe@163.com>
2026-01-16 17:50:28 +0800

    fix: login bugs

---
+++ Assets/RunFast2/Scripts/Controller/VersionUI.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace RunFast2.Scripts.Controller
+{
+    public class VersionUI : MonoBehaviour
+    {
+        public TMP_Text versionText;
+
+        private void Start()
+        {
+            versionText.text = Application.version;
+        }
+    }
+}
